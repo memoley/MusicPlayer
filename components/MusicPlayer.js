1b8c068122c3797775dbea0af9a9726db93e997b
Guillermo Leyva Castañón <42184356+memoley@users.noreply.github.com>
2024-05-05 21:41:41 -0600

    Mi primer comit

---
+++ components/MusicPlayer.js
@@ -0,0 +1,142 @@
+import React from "react";
+import {SafeAreaView, View, Text, StyleSheet, Dimensions, TouchableOpacity, Image} from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import Slider from "@react-native-community/slider";
+
+const {width, height} = Dimensions.get('window').width;
+
+const MusicPlayer=()=>{
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.mainContainer}>
+                <View style={styles.artworkWrapper}>
+                    <Image source={require('../assets/images/She Don`t Give A FO.jpg')} style={styles.artworkImage}/>
+                </View>
+                <View>
+                    <Text style={styles.title}>She Don't Give a FO</Text>
+                    <Text style={styles.artist}>Duki, Khea</Text>
+                </View>
+                <View>
+                    <Slider
+                        style={styles.progresContainer}
+                        values={10}
+                        minimumValue={0}
+                        maximumValue={100}
+                        minimumTrackTintColor="#FFD369"
+                        thumbTintColor="#FFD369"
+                        maximumTrackTintColor="#FFFFFF"
+                        onSlidingComplete={()=>{}}
+                    />
+                    <View style={styles.progressLabelContainer}>
+                        <Text style={styles.progressLabelText}>0:00</Text>
+                        <Text style={styles.progressLabelText}>3:35</Text>
+                    </View>
+                </View>
+                <View style={styles.musicControlls}>
+                        <TouchableOpacity onPress={()=>{}}>
+                            <Ionicons name="play-skip-back-outline" size={35} color="#FFD369" style={{marginTop:25}}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{}}>
+                            <Ionicons name="pause-circle" size={75} color="#FFD369" />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{}}>
+                            <Ionicons name="play-skip-forward-outline" size={35} color="#FFD369" style={{marginTop:25}}/>
+                        </TouchableOpacity>
+                    </View>
+            </View>
+            <View style={styles.bottomContainer}>
+                <View style={styles.bottomControls}>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name="heart-outline" size={30} color="#777777" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name="repeat" size={30} color="#777777" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name="share-outline" size={30} color="#777777" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name="ellipsis-horizontal" size={30} color="#777777" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+        
+    )
+};
+
+export default MusicPlayer;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#222831",
+    },
+    mainContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    bottomContainer:{
+        borderTopColor: '#393E46',
+        borderTopWidth: 1,
+        width: width,
+        alignItems: 'center',
+        paddingVertical: 15
+    },
+    bottomControls:{
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        width: '80%'
+    },
+    artworkWrapper:{
+        width: 300,
+        height: 340,
+        marginBottom: 25,
+        shadowColor: '#FFFFFF',
+        shadowOffset:{
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    artworkImage:{
+        width: '100%',
+        height: '100%',
+        borderRadius: 15
+    },
+    title:{
+        fontSize: 18,
+        fontWeight: '600',
+        textAlign: 'center',
+        color: '#EEEEEE',
+    },
+    artist:{
+        fontSize: 16,
+        fontWeight: '200',
+        textAlign: 'center',
+        color: '#EEEEEE',
+    },
+    progresContainer:{
+        width: 350,
+        height: 40,
+        flexDirection: 'row',
+        marginTop: 25
+    },
+    progressLabelContainer:{
+        width: 340,
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    },
+    progressLabelText:{
+        color: '#FFFFFF'
+    },
+    musicControlls:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        width: '60%',
+        marginTop: 15
+    }
+})
---
+++ model/data.js
@@ -0,0 +1,52 @@
+const songs =[
+    {
+        id:1,
+        title: "Remember me",
+        artist: "Duki, Khea, Bizarrap",
+        album: "Antes de Ameri",
+        image: require("../assets/images/Antes de Ameri.jpg"),
+        audio: require("../assets/audios/Remember Me.mp3")
+    },
+    {
+        id:2,
+        title: "Complicado",
+        artist: "Gera MX, Jayrick, Ervin River",
+        album: "No Teníamos Nada, Pero Éramos Felices",
+        image: require("../assets/images/No Teníamos Nada, Pero Éramos Felices.jpg"),
+        audio: require("../assets/audios/Complicado.mp3")
+    },
+    {
+        id:3,
+        title: "She Don't Give a FO",
+        artist: "Duki, Khea",
+        album: "She Don't Give a FO",
+        image: require("../assets/images/She Don`t Give A FO.jpg"),
+        audio: require("../assets/audios/She Don't Give a FO.mp3")
+    },
+    {
+        id:4,
+        title: "Wapo Traketero",
+        artist: "Nicki Nicole",
+        album: "Recuerdos",
+        image: require("../assets/images/Recuerdos.jpg"),
+        audio: require("../assets/audios/Wapo Traketero.mp3")
+    },
+    {
+        id:5,
+        title: "Bombas",
+        artist: "Alemán, Ximena Sariñana",
+        album: "Bombas",
+        image: require("../assets/images/Bombas.jpg"),
+        audio: require("../assets/audios/Bombas.mp3")
+    },
+    {
+        id:6,
+        title: "Querer Querernos",
+        artist: "Canserbero",
+        album: "Querer Querernos",
+        image: require("../assets/images/Querer Querernos.jpg"),
+        audio: require("../assets/audios/Remember Me.mp3")
+    }
+]
+
+export default songs;
